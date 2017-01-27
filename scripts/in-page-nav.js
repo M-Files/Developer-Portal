51d7022b78a5c5f240fe5ba692d031d6776e38a0
CraigHawker <craig.hawker@gmail.com>
2017-01-27 15:15:48 +0000

    enabled on-page navigation
    fixed some issues with layout

--- scripts/in-page-nav.js
+++ scripts/in-page-nav.js
@@ -1,17 +1,57 @@
 $(document).ready(function(){
 
     var $ul = $("<ul></ul>").attr("id", "in-page-nav");
-    var $headings = $("h1, h2, h3", $("article.page"));
+    var $headings = $("h2, h3", $("article.page"));
     
+    var headingLookups = [];
     $headings.each(function(i, o)
     {
         var $heading = $(this);
-        $ul.append($("<li></li>")
+        var $li = $("<li></li>")
             .addClass($heading.get(0).nodeName.toLowerCase())
-            .append($("<a></a>").attr("href", "#" + $heading.attr("id")).text($heading.text())));
-        
+            .append($("<a></a>").attr("href", "#" + $heading.attr("id")).text($heading.text()));
+        $ul.append($li);
+
+        headingLookups.push({
+            index: i,
+            heading: $heading,
+            offset: $heading.offset().top,
+            listItem: $li
+        })
     });
 
+    function reCalculateHeadingOffsets(){
+        $.each(headingLookups, function(i, o)
+        {
+            o.offset = o.heading.offset().top;
+        })
+    }
+    $(window).resize(reCalculateHeadingOffsets);
+
     $("BODY").append($ul);
+
+    $(document).on("scroll", function(){
+
+        // Go through headings and find the one we're scrolled to.
+        var lookup = null;
+        var position = $(document).scrollTop();
+        for(var i=0; i<headingLookups.length; i++)
+        {
+            if(position <= headingLookups[i].offset + 15)
+            {
+                lookup = headingLookups[i];
+                break;
+            }
+        }
+
+        // Remove any active nav items.
+        $(".in-page-nav-active").removeClass("in-page-nav-active");
+
+        // Set the current active flag.
+        lookup.listItem.addClass("in-page-nav-active");
+        lookup.heading.addClass("in-page-nav-active");
+
+    })
+    $(document).scroll();
 
 });
