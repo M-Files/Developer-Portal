7a54bee2a677a85e23daa6eec7b12843ce8d91e2
CraigHawker <craig.hawker@gmail.com>
2017-03-29 17:08:59 +0100

    back button closes sidebar

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,11 +1,48 @@
 (function($){
-    var $document = $(document);
     var $body = $("body");
-    $body.click(function(){
+
+    // Opens the sidebar.
+    function openSidebar()
+    {
+        // Show the sidebar.
+        $body.addClass("sidebar-expanded");
+
+        // Update the hash (add an entry to the history).
+        window.location.hash = "menu";
+    }
+
+    // Closes the sidebar.
+    function closeSidebar()
+    {
+        // Hide the sidebar.
         $body.removeClass("sidebar-expanded");
-    })
+
+        // If the hash is showing "menu", we can go back in the history to close it.
+        if(window.location.hash == "menu")
+        {
+            history.back();
+        }
+    }
+
+    // When the user clicks the body, close the sidebar.
+    $body.click(closeSidebar);
+
+    // When the sidebar icon is clicked, toggle the expanded sidebar.
     $("#hamburger").click(function(){
-        $body.toggleClass("sidebar-expanded");
+        
+        // If the sidebar is not shown shown...
+        if(false == $body.hasClass("sidebar-expanded"))
+        {
+            // Open the sidebar.
+            openSidebar();
+        }
+        else
+        {
+            // Close the sidebar.
+            closeSidebar();
+        }
+
         return false;
     });
+
 })(jQuery);
