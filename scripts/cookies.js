ebcf1453bd6796aaf73b5b8a495df831b0c9c867
CraigHawker <craig.hawker@gmail.com>
2018-04-17 12:29:30 +0100

    Fix in case of timezone issues on cookie expiry.

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -14,7 +14,7 @@ $(document).ready(function()
 	function setAcceptedCookie()
 	{
 		// Set the cookie
-		document.cookie = "accepted=1; path=/; expires=" + new Date(9999, 11, 31, 23, 59, 59).toUTCString();
+		document.cookie = "accepted=1; path=/; expires=" + new Date(9998, 11, 31, 23, 59, 59).toUTCString();
 		$body.addClass("accepted-cookies");
 	}
 	function getAcceptedCookie()
