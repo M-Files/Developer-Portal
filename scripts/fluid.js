9ab7f4fbb65a774b4c78725a45eff8e22022003d
Craig Hawker <craig.hawker@gmail.com>
2019-02-04 17:22:29 +0000

    Basic navigation reveal functionality. Fix for text colour in cookie warning (dark theme).

---
+++ scripts/fluid.js
@@ -0,0 +1,35 @@
+$(document).ready(function()
+{
+	$("aside#sidebar ul a")
+		.each(function(i, o)
+		{
+			var $this = $(this);
+			
+			// When we move, do something.
+			$this
+				.mousemove(function(e)
+				{
+					// Calculate the offset;
+					var offset = parseInt($this.offset().left);
+					var halfWidth = parseInt($this.outerWidth()) / 2;
+
+					// Calculate the delta.
+					var delta = (parseInt(e.clientX) - offset) - halfWidth;
+					// console.log({
+					// 	offset: offset,
+					// 	halfWidth: halfWidth,
+					// 	e: e,
+					// 	delta: delta
+					// });
+
+					// Update the CSS.
+					$this.css({
+						backgroundPositionX: delta + "px"
+					});
+				})
+				.mouseout(function()
+				{
+					$(this).css("background-position-x", "0px");
+				});
+		});
+});
