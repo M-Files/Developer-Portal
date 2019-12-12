4af67715fe49f96c4d57854c383a577db961e9aa
Craig Hawker <craig.hawker@gmail.com>
2019-12-12 11:21:13 +0000

    Optimisations for external links Optimisations for print layout

--- scripts/external-links.js
+++ scripts/external-links.js
@@ -1,25 +1,10 @@
 (function($){
-
-    // Create the "external link" icon.
-    var $externalLink = $("<span class='iconify' data-icon='mdi:open-in-new'></span>");
-
-    // Create the "github" icon.
-    var $githubLink = $("<span class='iconify' data-icon='mdi:github-circle'></span>");
-
     // Add the "external" class to all external links.
-    var $externalLinks = $("a", "article.page article")
+    $("a", "article.page article")
         .filter(function(){
             return this.hostname && this.hostname !== location.hostname;
 		})
 		.addClass("external")
         .attr("rel", "external noopener")
 		.attr("target", "_blank");
-    
-    // Add the github icon.
-    $externalLinks.filter(function(){ return this.hostname == "github.com" })
-        .prepend($githubLink.clone());
-    
-    // Add the other external icon.
-    $externalLinks.filter(function(){ return this.hostname != "github.com" })
-        .append($externalLink.clone());
 })(jQuery);
