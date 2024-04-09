070bff85dd956167f60a06c37bed61b5499b1c65
Craig Hawker <craig.hawker@m-files.com>
2024-04-09 18:04:48 +0100

    Lots of fixes to "inside page styles".

--- scripts/legacy.js
+++ scripts/legacy.js
@@ -1,30 +1,28 @@
 (function($){
-    $(document).ready(function(){
-		// Not legacy then die.
-		if(false == document.body.classList.contains("legacy"))
-			return;
+	// Not legacy then die.
+	if(false == document.body.classList.contains("legacy"))
+		return;
 
-		// We are viewing legacy content.  Get a reference to the legacy statement.
-		var $legacyStatement = $("div.legacy");
-		
-		// Shows the lightbox-style full-screen legacy statement.
-		function fullscreenLegacy(){
-			$legacyStatement.addClass("shown");
-		}
+	// We are viewing legacy content.  Get a reference to the legacy statement.
+	var $legacyStatement = $("body, div.legacy");
+	
+	// Shows the lightbox-style full-screen legacy statement.
+	function fullscreenLegacy(){
+		$legacyStatement.addClass("shown");
+	}
 
-		// Collapses the legacy statement.
-		function collapseLegacy()
-		{
-			$legacyStatement.removeClass("shown");
-		}
+	// Collapses the legacy statement.
+	function collapseLegacy()
+	{
+		$legacyStatement.removeClass("shown");
+	}
 
-		// When the close button is clicked, collapse the statement.
-		$("button.close", $legacyStatement).click(collapseLegacy)
+	// When the close button is clicked, collapse the statement.
+	$("button.close", $legacyStatement).click(collapseLegacy)
 
-		// When the background of the overlay is clicked, collapse the statement.
-		$legacyStatement.click(collapseLegacy);
+	// When the background of the overlay is clicked, collapse the statement.
+	$legacyStatement.click(collapseLegacy);
 
-		// By default show the statement.
-		fullscreenLegacy();
-    });
+	// By default show the statement.
+	fullscreenLegacy();
 })(jQuery);
