13984833a6db7458e3d030aae6039ea35a85eb85
Craig Hawker <craig.hawker@m-files.com>
2024-04-22 17:55:42 +0100

    removed headings from search

--- Search/search.js
+++ Search/search.js
@@ -45,7 +45,7 @@ window.executeSearch = (function(){
   
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
-        appendString += '<li><h3><a href="' + item.url + '">' + item.title + '</a></h3>';
+        appendString += '<li><a href="' + item.url + '">' + item.title + '</a>';
         appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
       }
   
